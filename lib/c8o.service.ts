ea0e6e00b0395a75f31b7b0b6dcb51c82423a486
Charles GRIMONT <grimont.c@gmail.com>
2022-09-19 14:16:32 +0200

    remove accept gzip (auto added by browser)

--- lib/c8o.service.ts
+++ lib/c8o.service.ts
@@ -15,7 +15,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "3.0.18-beta2";
+        return "4.0.18-beta4";
     }
     /**
      * Method to bastract http get
