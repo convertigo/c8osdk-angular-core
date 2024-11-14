0a87ce0dc15573b0c00d6a780a46b5cefd19044b
Charles GRIMONT <grimont.c@gmail.com>
2024-11-14 14:39:24 +0100

    updated pouchdb-quick-search-convertigo

--- lib/c8o.service.ts
+++ lib/c8o.service.ts
@@ -15,7 +15,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "4.0.23";
+        return "4.0.24";
     }
     /**
      * Method to bastract http get
