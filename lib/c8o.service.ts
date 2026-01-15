7902b5863ba5684796a2837520504df29542f3ac
Charles Grimont <charlesg@convertigo.com>
2026-01-15 16:48:24 +0100

    update sdk version to 4.0.26-beta5 and format code in c8o.service.ts

--- lib/c8o.service.ts
+++ lib/c8o.service.ts
@@ -15,7 +15,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "4.0.25-beta1";
+        return "4.0.26-beta5";
     }
     /**
      * Method to bastract http get
